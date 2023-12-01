5e3ad457fd399e325cf654ba671209f910cf1a64
motohiro-mm <polarbear.dx@gmail.com>
2023-12-01 22:20:49 +0900

    ls_without_l_optionsメソッドを作成。

--- 07.ls_object/lib/ls_command.rb
+++ 07.ls_object/lib/ls_command.rb
@@ -13,4 +13,48 @@ class LsCommand
     files.reverse! if @options[:r]
     files
   end
+
+  def ls_files(terminal_width)
+    @options[:l] ? ls_with_l_option : ls_without_l_option(terminal_width)
+  end
+
+  def ls_with_l_option
+  end
+
+  def ls_without_l_option(terminal_width)
+    filename_width = file_width(files)
+    transposed_files = transpose_files(terminal_width)
+    transposed_files.map! do |transposed_file|
+      transposed_file.map! do |file|
+        if file == transposed_file[-1]
+          file.ljust(filename_width, ' ') + "\n"
+        else
+          file.ljust(filename_width, ' ')
+        end
+      end.join
+    end.join.chomp
+  end
+
+  def transpose_files(terminal_width)
+    files_count = @files.count
+    files_column(@files, terminal_width)
+    lines = (files_count.to_f / files_column(@files, terminal_width)).ceil
+    separated_files = @files.each_slice(lines).to_a
+    separated_files[0].zip(*separated_files[1..-1]).map(&:compact)
+  end
+
+  def file_width(files)
+    filename_lengths = files.map(&:length)
+    (filename_lengths.max.to_f/8).ceil*8
+  end
+
+  MAX_COLUMN = 3
+  def files_column(files, terminal_width)
+    filename_width = file_width(files)
+    if filename_width * MAX_COLUMN > terminal_width
+      filename_width >= terminal_width ? 1 : terminal_width / filename_width
+    else
+      MAX_COLUMN
+    end
+  end
 end
--- 07.ls_object/test/ls_command_test.rb
+++ 07.ls_object/test/ls_command_test.rb
@@ -5,11 +5,53 @@ require_relative '../lib/ls_command'
 require_relative '../lib/file_status'
 
 class LsCommandTest < Minitest::Test
-  def test_files
-    options = { a: true, l: false, r: false }
-    assert_equal ['.', '..', '.gitkeep', 'fizzbuzz.rb'], LsCommand.new('../01.fizzbuzz', options).files
+  def setup
+    options = { a: false, l: false, r: false }
+    @ls_command = LsCommand.new('..', options)
+  end
 
-    options2 = { a: false, l: false, r: false }
-    assert_equal ['fizzbuzz.rb'], LsCommand.new('../01.fizzbuzz', options2).files
+  def test_ls_command_files
+    a_option = { a: true, l: false, r: false }
+    assert_equal ['.', '..', '.gitkeep', 'fizzbuzz.rb'], LsCommand.new('../01.fizzbuzz', a_option).files
+
+    a_r_options = { a: true, l: false, r: true }
+    assert_equal ['fizzbuzz.rb', '.gitkeep', '..', '.'], LsCommand.new('../01.fizzbuzz', a_r_options).files
+  end
+
+  def test_ls_files_without_l_terminal_width_86
+    example = <<~TEXT.chomp
+    01.fizzbuzz             05.wc                   99.wc_object            
+    02.calendar             06.bowling_object       README.md               
+    03.bowling              07.ls_object            
+    04.ls                   98.rake                 
+    TEXT
+    assert_equal example, @ls_command.ls_files(86)
+  end
+
+  def test_ls_files_without_l_terminal_width_71
+    example = <<~TEXT.chomp
+    01.fizzbuzz             06.bowling_object       
+    02.calendar             07.ls_object            
+    03.bowling              98.rake                 
+    04.ls                   99.wc_object            
+    05.wc                   README.md               
+    TEXT
+    assert_equal example, @ls_command.ls_files(71)
+  end
+
+  def test_ls_files_without_l_terminal_width_47
+    example = <<~TEXT.chomp
+    01.fizzbuzz             
+    02.calendar             
+    03.bowling              
+    04.ls                   
+    05.wc                   
+    06.bowling_object       
+    07.ls_object            
+    98.rake                 
+    99.wc_object            
+    README.md               
+    TEXT
+    assert_equal example, @ls_command.ls_files(47)
   end
 end
